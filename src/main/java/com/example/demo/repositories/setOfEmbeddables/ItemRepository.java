f3457562d6421d2f65d109404b453e62c77a5b29
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-21 21:19:39 -0300

    repository

---
+++ src/main/java/com/example/demo/repositories/setOfEmbeddables/ItemRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repositories.setOfEmbeddables;
+
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.setOfEmbeddables.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select i from Item i inner join fetch i.images where i.id = :id")
+    Item findItemWithImages(@Param("id") Long id);
+
+    @Query(value = "SELECT FNAME FROM IMAGE WHERE ITEM_ID = ?1",
+            nativeQuery = true)
+    Set<String> findImagesNative(Long id);
+}
