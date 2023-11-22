e0ae3463491cc06cfa37eb0c24a95a92995b8c0d
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-21 21:41:37 -0300

    building image

---
+++ src/main/java/com/example/demo/mapOfEmbeddables/Image.java
@@ -0,0 +1,69 @@
+package com.example.demo.mapOfEmbeddables;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Image {
+
+	@Column(nullable = true) // Can be null, not part of PK!
+    private String title;
+
+    @NotNull
+    private int width;
+
+    @NotNull
+    private int height;
+
+    public Image() {
+    }
+
+    public Image(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    // Whenever value-types are managed in collections, overriding equals/hashCode is a good idea!
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return width == image.width &&
+                height == image.height &&
+                Objects.equals(title, image.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, width, height);
+    }
+}
