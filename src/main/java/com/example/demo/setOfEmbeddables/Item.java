c41086366be2a562841005f88a19072be6079f7f
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-21 21:10:51 -0300

    constants and item model

--- src/main/java/com/example/demo/setOfEmbeddables/Item.java
+++ src/main/java/com/example/demo/setOfEmbeddables/Item.java
@@ -1,5 +1,58 @@
 package com.example.demo.setOfEmbeddables;
 
+import java.awt.Image;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.example.demo.Constants;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Item {
 
+	@Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    private Long id;
+
+    private String name;
+
+    @ElementCollection
+    @CollectionTable(name = "IMAGE")
+    @AttributeOverride(
+            name = "filename",
+            column = @Column(name = "FNAME", nullable = false)
+    )
+    private Set<Image> images = new HashSet<>();
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Image> getImages() {
+        return Collections.unmodifiableSet(images);
+    }
+
+    public void addImage(Image image) {
+        images.add(image);
+    }
 }
