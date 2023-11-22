d19fa21cdcfd143305379db7b7213116753a9607
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-21 21:14:54 -0300

    package info

--- src/main/java/com/example/demo/setOfEmbeddables/package-info.java
+++ src/main/java/com/example/demo/setOfEmbeddables/package-info.java
@@ -1,1 +1,16 @@
+
+@org.hibernate.annotations.GenericGenerator( // un generador CUSTOM de valor automático, donde la estrategia ahora se llama "type"
+  name = "ID_GENERATOR",
+  //strategy = "enhanced-sequence",
+  type = org.hibernate.id.enhanced.SequenceStyleGenerator.class, // cambió, ahora debemos setearlo de esta manera
+  parameters = {
+     @org.hibernate.annotations.Parameter(
+        name = "sequence_name",
+        value = "SETTED_BY_ME_SEQUENCE"
+     ),
+     @org.hibernate.annotations.Parameter(
+        name = "initial_value",
+        value = "1616"
+     )
+})
 package com.example.demo.setOfEmbeddables;
